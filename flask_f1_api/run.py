196b119eac8a9a624c1f0892314859cb3390f242
Alan <alanmarsh99@outlook.com>
2024-11-06 21:44:24 +0000

    updated views to add simple caching for each view to improve user xp

--- flask_f1_api/run.py
+++ flask_f1_api/run.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
+from app.cache import cache
 
 app = Flask(__name__)
 
 # Enable CORS for the entire app
 CORS(app)
 
-# Import blueprint
-from app.views import api_blueprint
+# Set up cache configuration
+app.config['CACHE_TYPE'] = 'SimpleCache'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+cache.init_app(app)
 
-# Register blueprint
+# Blueprint
+from app.views import api_blueprint
 app.register_blueprint(api_blueprint)
 
 if __name__ == '__main__':
