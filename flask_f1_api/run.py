7ea7077d2ff461772588cb2c5708ced0f2bd2838
Alan <alanmarsh99@outlook.com>
2024-11-07 13:41:34 +0000

    updated caching to store caches in Upstash Redis database to improve load times

--- flask_f1_api/run.py
+++ flask_f1_api/run.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from flask_cors import CORS
 from app.cache import cache
+from redis import SSLConnection
 
 app = Flask(__name__)
 
 # Enable CORS for the entire app
 CORS(app)
 
-# Set up cache configuration
-app.config['CACHE_TYPE'] = 'SimpleCache'
+# Set up cache configuration for Redis using Upstash
+app.config['CACHE_TYPE'] = 'RedisCache'
 app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+
+# Hardcode the Upstash Redis URL for now
+
+app.config['CACHE_REDIS_URL'] = 'rediss://:AWMkAAIjcDFhNDk4NTcwMjI4NTg0ZTNmYjZjMjBiNzdhODIzNmNmMXAxMA@careful-gobbler-25380.upstash.io:6379'
+
+# Ensure SSL/TLS is used for the Redis connection
+app.config['CACHE_OPTIONS'] = {
+    'connection_class': SSLConnection,  # Use SSLConnection for secure connection
+}
+
+# Initialize the cache with the app
 cache.init_app(app)
 
 # Blueprint
