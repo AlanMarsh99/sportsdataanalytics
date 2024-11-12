d9fa9a43b2c3e4c9456a98f4a29e9bcbb9de01b7
deivit21 <david29.st@gmail.com>
2024-11-12 19:09:00 +0000

    Log in and sign up

--- frontend/lib/core/shared/globals.dart
+++ frontend/lib/core/shared/globals.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/core/models/user_app.dart';
 import 'package:frontend/ui/theme.dart';
 
 import 'package:intl/intl.dart';
 
 class Globals {
-
   static String baseUrl = 'http://127.0.0.1:8000/api/v1';
+
+  // Firebase Auth Exceptions
+  static const String SUCCESS = 'success';
+  static const String WEAK_PASSWORD = 'weak-password';
+  static const String EMAIL_ALREADY_IN_USE = 'email-already-in-use';
+  static const String UNCLASSIFIED_ERROR = 'unclassified-error';
+  static const String USER_MISMATCH = 'user-mismatch';
+  static const String USER_NOT_FOUND = 'user-not-found';
+  static const String INVALID_CREDENTIAL = 'invalid-credential';
+  static const String INVALID_EMAIL = 'invalid-email';
+  static const String WRONG_PASSWORD = 'wrong-password';
+  static const String REQUIRES_RECENT_LOGIN = 'requires-recent-login';
+
+  static final Map models = {
+    UserApp: (data) => UserApp.fromMap(data),
+  };
 
   static String toLocalDate(DateTime d) {
     String date = DateFormat('dd/MM/yyyy HH:mm').format(d);
---
+++ frontend/lib/ui/widgets/dialogs/warning_error_dialog.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/ui/theme.dart';
+
+class WarningDialog extends StatelessWidget {
+  final String message;
+  final bool error;
+
+  WarningDialog({required this.message, required this.error});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        title: error
+            ? const Text(
+                "ERROR",
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.red),
+              )
+            : const Text(
+                "WARNING",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+        content: Text(message, style: const TextStyle(color: Colors.black)),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text(
+              "OK",
+              style: TextStyle(color: primary),
+            ),
+          ),
+        ]);
+  }
+}
