357f65f9921feeb2a9ce3e1ce1d28f2219ccf447
deivit21 <david29.st@gmail.com>
2024-11-18 14:55:08 +0000

    Changed some images

--- frontend/lib/core/shared/globals.dart
+++ frontend/lib/core/shared/globals.dart
@@ -23,6 +23,45 @@ class Globals {
     UserApp: (data) => UserApp.fromMap(data),
   };
 
+  static final Map<String, String> teamLogos = {
+    'Alpine F1 Team': 'assets/teams/logos/alpine-logo.png',
+    'Aston Martin': 'assets/teams/logos/aston-martin-logo.png',
+    'Ferrari': 'assets/teams/logos/ferrari-logo.png',
+    'Haas F1 Team': 'assets/teams/logos/haas-logo.png',
+    'McLaren': 'assets/teams/logos/mclaren-logo.png',
+    'Mercedes': 'assets/teams/logos/mercedes-logo.png',
+    'RB F1 Team': 'assets/teams/logos/rb-logo.png',
+    'Red Bull': 'assets/teams/logos/red-bull-logo.png',
+    'Sauber': 'assets/teams/logos/sauber-logo.png',
+    'Williams': 'assets/teams/logos/williams-logo.png',
+  };
+
+  static final Map<String, String> teamBadges = {
+    'Red Bull': 'assets/teams/badges/red-bull-badge.png',
+    'Mercedes': 'assets/teams/badges/mercedes-badge.png',
+    'Ferrari': 'assets/teams/badges/ferrari-badge.png',
+    'McLaren': 'assets/teams/badges/mclaren-badge.png',
+    'Aston Martin': 'assets/teams/badges/aston-martin-badge.png',
+    'Alpine F1 Team': 'assets/teams/badges/alpine-badge.png',
+    'Williams': 'assets/teams/badges/williams-badge.png',
+    'Haas F1 Team': 'assets/teams/badges/haas-badge.png',
+    'Sauber': 'assets/teams/badges/sauber-badge.png',
+    'RB F1 Team': 'assets/teams/badges/rb-badge.png',
+  };
+
+  static const Map<String, String> teamNameMapping = {
+    'Red Bull Racing': 'Red Bull',
+    'Scuderia Ferrari': 'Ferrari',
+    'Mercedes AMG Petronas': 'Mercedes',
+    'McLaren F1 Team': 'McLaren',
+    'Aston Martin': 'Aston Martin',
+    'Alpine F1 Team': 'Alpine F1 Team',
+    'Williams Racing': 'Williams',
+    'Haas F1 Team': 'Haas F1 Team',
+    'Alfa Romeo': 'Sauber',
+    'AlphaTauri': 'RB F1 Team',
+  };
+
   static String toLocalDate(DateTime d) {
     String date = DateFormat('dd/MM/yyyy HH:mm').format(d);
     return date;
