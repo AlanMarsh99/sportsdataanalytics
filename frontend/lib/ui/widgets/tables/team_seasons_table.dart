d8f6fe596bb1da5304a6693ccddb75aec5a9eb2c
Alan <alanmarsh99@outlook.com>
2024-12-04 21:44:44 +0000

    added filtering to teams tables

--- frontend/lib/ui/widgets/tables/team_seasons_table.dart
+++ frontend/lib/ui/widgets/tables/team_seasons_table.dart
@@ -1,130 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/core/models/team_season_result.dart';
 
-class TeamSeasonsTable extends StatelessWidget {
+class TeamSeasonsTable extends StatefulWidget {
   const TeamSeasonsTable({Key? key, required this.seasonsData})
       : super(key: key);
 
   final List<dynamic> seasonsData;
 
   @override
-  Widget build(BuildContext context) {
-    List<TeamSeasonResult> teamSeasons =
-        seasonsData.map((json) => TeamSeasonResult.fromJson(json)).toList();
-    return Scrollbar(
-      thumbVisibility: true,
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Scrollbar(
-          thumbVisibility: true,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: DataTable(
-                columns: const [
-                  DataColumn(
-                    label: Text(
-                      'Year',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Position',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Points',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Wins',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Podiums',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Pole positions',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Drivers',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-                ],
-                rows: teamSeasons.map((teamSeasonResult) {
-                  return DataRow(cells: [
-                    DataCell(
-                      Text(
-                        teamSeasonResult.year,
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    DataCell(
-                      _buildPositionContainer(teamSeasonResult.position),
-                    ),
-                    DataCell(
-                      Text(
-                        teamSeasonResult.points.toString(),
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    DataCell(
-                      Text(
-                        teamSeasonResult.wins.toString(),
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    DataCell(
-                      Text(
-                        teamSeasonResult.podiums.toString(),
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    DataCell(
-                      Text(
-                        teamSeasonResult.polePositions.toString(),
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    DataCell(
-                      Text(
-                        teamSeasonResult.driversList.join(', '),
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  ]);
-                }).toList(),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+  _TeamSeasonsTableState createState() => _TeamSeasonsTableState();
+}
+
+class _TeamSeasonsTableState extends State<TeamSeasonsTable> {
+  List<TeamSeasonResult> teamSeasonsList = [];
+  List<TeamSeasonResult> filteredTeamSeasonsList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    teamSeasonsList = widget.seasonsData
+        .map((json) => TeamSeasonResult.fromJson(json))
+        .toList();
+    filteredTeamSeasonsList = List.from(teamSeasonsList);
+  }
+
+  void updateFilter(String value) {
+    setState(() {
+      final filter = value.toLowerCase();
+      filteredTeamSeasonsList = teamSeasonsList.where((teamSeasonResult) {
+        final yearMatch = teamSeasonResult.year.toLowerCase().contains(filter);
+        final positionMatch =
+            teamSeasonResult.position.toLowerCase().contains(filter);
+        final pointsMatch =
+            teamSeasonResult.points.toString().contains(filter);
+        final winsMatch = teamSeasonResult.wins.toString().contains(filter);
+        final podiumsMatch =
+            teamSeasonResult.podiums.toString().contains(filter);
+        final polePositionsMatch =
+            teamSeasonResult.polePositions.toString().contains(filter);
+        final driversMatch = teamSeasonResult.driversList
+            .any((driver) => driver.toLowerCase().contains(filter));
+
+        return yearMatch ||
+            positionMatch ||
+            pointsMatch ||
+            winsMatch ||
+            podiumsMatch ||
+            polePositionsMatch ||
+            driversMatch;
+      }).toList();
+    });
   }
 
   Widget _buildPositionContainer(String position) {
@@ -166,6 +91,159 @@ class TeamSeasonsTable extends StatelessWidget {
           style: TextStyle(color: pos <= 3 ? Colors.white : Colors.black),
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Filtering TextField
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: TextField(
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              hintText: 'Enter year, position, driver, etc.',
+              hintStyle: const TextStyle(color: Colors.white70),
+              labelText: 'Filter team seasons',
+              labelStyle: const TextStyle(color: Colors.white),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: const BorderSide(color: Colors.white),
+              ),
+              prefixIcon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+            onChanged: updateFilter,
+          ),
+        ),
+        Expanded(
+          child: Scrollbar(
+            thumbVisibility: true,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Scrollbar(
+                thumbVisibility: true,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: DataTable(
+                      columns: const [
+                        DataColumn(
+                          label: Text(
+                            'Year',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Position',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Points',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Wins',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Podiums',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Pole positions',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Drivers',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                      ],
+                      rows: filteredTeamSeasonsList.map((teamSeasonResult) {
+                        return DataRow(cells: [
+                          DataCell(
+                            Text(
+                              teamSeasonResult.year,
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          DataCell(
+                            _buildPositionContainer(teamSeasonResult.position),
+                          ),
+                          DataCell(
+                            Text(
+                              teamSeasonResult.points.toString(),
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              teamSeasonResult.wins.toString(),
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              teamSeasonResult.podiums.toString(),
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              teamSeasonResult.polePositions.toString(),
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              teamSeasonResult.driversList.join(', '),
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ]);
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
