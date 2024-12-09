90c088f31c53394cacd7b65f8a9fa91af8398cdf
Alan <alanmarsh99@outlook.com>
2024-12-09 16:12:32 +0000

    fixed scrollbar not working on lap comparison table

--- frontend/lib/ui/widgets/tables/lap_comparison_table.dart
+++ frontend/lib/ui/widgets/tables/lap_comparison_table.dart
@@ -12,134 +12,153 @@ class LapComparisonTable extends StatelessWidget {
     required this.driver2LapData,
   }) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     final lapCount = driver1LapData.laps.length;
     final isMobile = Responsive.isMobile(context);
-    return Scrollbar(
-      thumbVisibility: true,
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Scrollbar(
-          thumbVisibility: true,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: DataTable(
-                columnSpacing: isMobile ? 15 : 56,
-                columns: [
-                   DataColumn(
-                    label: Text(
-                      'Lap',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(isMobile ? '${driver1LapData.driverName.split(' ').last}\nPosition' :
-                      '${driver1LapData.driverName.split(' ').last} Position',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      isMobile ? 'Lap Time' : '${driver1LapData.driverName.split(' ').last} Lap Time',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Difference',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                       isMobile ? 'Lap Time' : '${driver2LapData.driverName.split(' ').last} Lap Time',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(isMobile ? '${driver2LapData.driverName.split(' ').last}\nPosition' :
-                      '${driver2LapData.driverName.split(' ').last} Position',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ],
-                rows: List<DataRow>.generate(
-                  lapCount,
-                  (index) {
-                    final lap1 = driver1LapData.laps[index];
-                    final lap2 = driver2LapData.laps[index];
-                    final timeDiff =
-                        _calculateTimeDifference(lap1.lapTime, lap2.lapTime);
 
-                    return DataRow(
-                      cells: [
-                         DataCell(
-                          Text(
-                            (index+1).toString(),
-                            style: const TextStyle(color: Colors.black),
-                          ),
+    // Create separate controllers for vertical and horizontal scrolling
+    final verticalController = ScrollController();
+    final horizontalController = ScrollController();
+
+    return SizedBox(
+      height: 400, // Constrain height so overflow occurs, allowing scroll
+      child: Scrollbar(
+        thumbVisibility: true,
+        controller: verticalController,
+        child: SingleChildScrollView(
+          controller: verticalController,
+          scrollDirection: Axis.vertical,
+          child: Scrollbar(
+            thumbVisibility: true,
+            controller: horizontalController,
+            notificationPredicate: (notification) => notification.depth == 1,
+            child: SingleChildScrollView(
+              controller: horizontalController,
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: DataTable(
+                  columnSpacing: isMobile ? 15 : 56,
+                  columns: [
+                    DataColumn(
+                      label: Text(
+                        'Lap',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
                         ),
-                        DataCell(
-                          Text(
-                            lap1.position.toString(),
-                            style: const TextStyle(color: Colors.black),
-                          ),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        isMobile
+                            ? '${driver1LapData.driverName.split(' ').last}\nPosition'
+                            : '${driver1LapData.driverName.split(' ').last} Position',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
                         ),
-                        DataCell(
-                          Text(
-                            lap1.lapTime,
-                            style: const TextStyle(color: Colors.black),
-                          ),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        isMobile
+                            ? 'Lap Time'
+                            : '${driver1LapData.driverName.split(' ').last} Lap Time',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
                         ),
-                        DataCell(
-                          Text(
-                            timeDiff,
-                            style: TextStyle(
-                              color: _getDifferenceColour(timeDiff),
-                              fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Difference',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        isMobile
+                            ? 'Lap Time'
+                            : '${driver2LapData.driverName.split(' ').last} Lap Time',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        isMobile
+                            ? '${driver2LapData.driverName.split(' ').last}\nPosition'
+                            : '${driver2LapData.driverName.split(' ').last} Position',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                  rows: List<DataRow>.generate(
+                    lapCount,
+                    (index) {
+                      final lap1 = driver1LapData.laps[index];
+                      final lap2 = driver2LapData.laps[index];
+                      final timeDiff = _calculateTimeDifference(lap1.lapTime, lap2.lapTime);
+
+                      return DataRow(
+                        cells: [
+                          DataCell(
+                            Text(
+                              (index + 1).toString(),
+                              style: const TextStyle(color: Colors.black),
                             ),
                           ),
-                        ),
-                        DataCell(
-                          Text(
-                            lap2.lapTime,
-                            style: const TextStyle(color: Colors.black),
+                          DataCell(
+                            Text(
+                              lap1.position.toString(),
+                              style: const TextStyle(color: Colors.black),
+                            ),
                           ),
-                        ),
-                        DataCell(
-                          Text(
-                            lap2.position.toString(),
-                            style: const TextStyle(color: Colors.black),
+                          DataCell(
+                            Text(
+                              lap1.lapTime,
+                              style: const TextStyle(color: Colors.black),
+                            ),
                           ),
-                        ),
-                      ],
-                    );
-                  },
+                          DataCell(
+                            Text(
+                              timeDiff,
+                              style: TextStyle(
+                                color: _getDifferenceColour(timeDiff),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              lap2.lapTime,
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              lap2.position.toString(),
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
